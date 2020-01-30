bc5bffa35a47a2a1f6c080ca938d4adc453130f9
Berkay Sağman <brkysgmn@gmail.com>
2020-01-30 19:28:42 +0300

    Unity files

---
+++ Square Wars/Assets/Scripts/CellController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnMouseDown() 
+    {
+        FindObjectOfType<Region>().AddCellToPlayerRegion(GetCell());
+    }
+    public GameObject GetCell()
+    {
+        return gameObject;
+    }
+    public void ChangeCellColor()
+    {
+
+    }
+}
---
+++ Square Wars/Assets/Scripts/Region.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Region : MonoBehaviour
+{
+    [SerializeField] List<GameObject> region = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Build()
+    {
+        
+    }
+    public void AddCellToPlayerRegion(GameObject selectedCell)
+    {
+        if(!region.Contains(selectedCell))
+        {
+            Select(selectedCell);
+            
+        }
+        else
+        {
+            Deselect(selectedCell);
+        }
+    }
+
+    private void Select(GameObject selectedCell)
+    {
+        region.Add(selectedCell);
+        foreach (var cells in region)
+        {
+            cells.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    private void Deselect(GameObject selectedCell)
+    {
+        selectedCell.GetComponent<SpriteRenderer>().color = Color.white;
+        region.Remove(selectedCell);
+    }
+
+    public void CompleteTakeover()
+    {
+
+    }
+}
